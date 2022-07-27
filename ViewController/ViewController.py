49c69ac5b72b38fd14aec459c6c26d6750336e00
Jacob Woods <68571928+jacobwoods45@users.noreply.github.com>
2022-07-26 21:41:42 -0500

    Create ViewController.py

---
+++ ViewController/ViewController.py
@@ -0,0 +1,8 @@
+'''
+Function to generate cost matrix for flights
+Input: none
+Output: Returns a 12 x 4 matrix of prices
+'''
+def get_cost_matrix():
+    cost_matrix = [[100, 75, 50, 100] for row in range(12)]
+    return cost_matrix
